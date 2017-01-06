dd2a9afb929a4f44a89489a51c7df15ee4588755
Wingo <cmwingo@gmail.com>
2017-01-06 10:25:31 -0800

    Display function will now correctly display the name attribute of the doctor object

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,8 +1,17 @@
 var Doctors = require('./../js/doctor-lookup.js').getDoctors;
 
+function displayDoctors(doctors) {
+  $('.showDoctors').text(function() {
+    doctors.forEach(function(doctor) {
+      $('.showDoctors').append(doctor.name + "<br>");
+    });
+  });
+}
+
 $(document).ready(function() {
   $('#medicalIssue').click(function() {
     var medicalIssue = $('#issue').val();
-    var doctors = new Array(Doctors(medicalIssue));
+    var doctors = new Array(Doctors(medicalIssue, displayDoctors));
+    console.log("Front end:" + doctors);
   });
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,11 +1,23 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getDoctors = function(medicalIssue) {
+function Doctor(name) {
+  this.name = name;
+}
+
+exports.getDoctors = function(medicalIssue, displayFunction) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
+      var doctors = new Array();
+      result.data.forEach(function(dataObject, i) {
+        doctors[i] = new Doctor(dataObject.practices[0].name);
+      });
+      displayFunction(doctors);
       console.log(result);
+      console.log(doctors[0].name);
+      return result;
     })
    .fail(function(error){
       console.log("fail");
+      return error;
     });
 };
